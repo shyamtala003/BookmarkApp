7fafd9e2a4dab17d7f96ce894617d4a7321cc7a4
shyamtala003 <shyamtala003@gmail.com>
2022-09-30 21:07:43 +0530

    bug fixes

--- main.js
+++ main.js
@@ -79,8 +79,8 @@ let saveBookmark = () => {
     let url = document.getElementById('url');
     localStorage.setItem(title.value, url.value);
 
-    title.setAttribute("value","");
-    url.setAttribute("value","");
+    title.value="";
+    url.value="";
 
     dropDownForm.classList.add("drop__down__close");
     dropDownForm.classList.remove("drop__down__show")
